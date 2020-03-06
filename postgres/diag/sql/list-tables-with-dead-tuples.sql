7ef2045b965da096c4fdf566ca86a512159430f2
nasbyj <45640492+nasbyj@users.noreply.github.com>
2020-03-06 12:38:14 -0600

    Change dead-tuples script to return FQN
    relname is not so useful in a database with multiple schemas. Return a fully qualified name instead.
    
    While we're at it, protect against name conflicts in seach_path.

--- postgres/diag/sql/list-tables-with-dead-tuples.sql
+++ postgres/diag/sql/list-tables-with-dead-tuples.sql
@@ -18,15 +18,15 @@
 
 
 select                         
-       sat.relname
+       pg_catalog.quote_ident(sat.schemaname) || '.' || pg_catalog.quote_ident(sat.relname) AS table_name
       ,sat.n_dead_tup
       ,sat.n_live_tup  
-      ,to_char(sat.last_autovacuum, 'YYYY-MM-DD HH24:MI:SS') last_autovacuum
+      ,pg_catalog.to_char(sat.last_autovacuum, 'YYYY-MM-DD HH24:MI:SS') last_autovacuum
       ,sat.autovacuum_count
-      ,to_char(sat.last_vacuum, 'YYYY-MM-DD HH24:MI:SS') last_vacuum
+      ,pg_catolog.to_char(sat.last_vacuum, 'YYYY-MM-DD HH24:MI:SS') last_vacuum
       ,sat.vacuum_count
       ,sat.seq_scan
       ,sat.idx_scan
-  from pg_stat_all_tables sat
+  from pg_catalog.pg_stat_all_tables sat
  where sat.n_dead_tup != 0
 order by sat.n_dead_tup desc;
